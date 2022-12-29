a6ad3dc9454844bdad9e98aa0f34959cc9e2d446
Greta Kisunaite <greta.kisunaite@gmail.com>
2022-12-29 20:40:25 +0200

    Init fourth part.

---
+++ Part4/Income.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Income extends Transaction {
+    private boolean isBankTransaction;
+
+    public Income(String id, float amount, LocalDate date, String category, String type, boolean isBankTransaction, String additionalInformation) {
+        super(id, amount, date, category, type, additionalInformation);
+        this.isBankTransaction = isBankTransaction;
+    }
+
+    public Income(float amount, LocalDate date, String category, String type, boolean isBankTransaction, String additionalInformation) {
+        super(amount, date, category, type, additionalInformation);
+        this.isBankTransaction = isBankTransaction;
+    }
+
+    public boolean isBankTransaction() {
+        return isBankTransaction;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{id=%s, amount=%.2f, date=%s, category=%s, type=%s, isBankTransaction=%b, additionalInformation=%s}",
+            super.getId(), super.getAmount(), super.getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+            super.getCategory(), super.getType(), isBankTransaction, super.getAdditionalInformation());
+    }
+}
---
+++ Part4/Program.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean runProgram = true;
+        Budget budget = new Budget();
+
+        Printer.programStartGreeting();
+
+        while (runProgram) {
+            Printer.options();
+
+            String inputOption = sc.nextLine().toLowerCase();
+
+            switch (inputOption) {
+                case "x" -> runProgram = false;
+                case "ii" -> addIncome(sc, budget);
+                case "ie" -> addExpense(sc, budget);
+                case "d" -> deleteTransaction(sc, budget);
+                case "e" -> editTransaction(sc, budget);
+                case "b" -> Printer.printBudget(budget.balance());
+                case "o" -> printAllTransactions(budget);
+                case "oi" -> Printer.print(budget.getIncomes(), "Incomes");
+                case "oe" -> Printer.print(budget.getExpenses(), "Expenses");
+                default -> Printer.invalidArgumentMessage();
+            }
+        }
+        sc.close();
+
+    }
+
+    private static void addIncome(Scanner sc, Budget budget) {
+        InputProcessor inputProcessor = new InputProcessor(sc);
+        Income income = inputProcessor.getIncome();
+        budget.addTransaction(income);
+    }
+
+    private static void addExpense(Scanner sc, Budget budget) {
+        InputProcessor inputProcessor = new InputProcessor(sc);
+        Expense expense = inputProcessor.getExpense();
+        budget.addTransaction(expense);
+    }
+
+    private static void deleteTransaction(Scanner sc, Budget budget) {
+        InputProcessor inputProcessor = new InputProcessor(sc, budget);
+        Printer.deleteMessage();
+
+        Transaction transaction = inputProcessor.getTransactionById();
+        budget.deleteTransaction(transaction);
+    }
+
+    private static void editTransaction(Scanner sc, Budget budget) {
+        if (budget.getExpenses().size() + budget.getIncomes().size() == 0) {
+            Printer.noTransactionsMessage();
+            return;
+        }
+        InputProcessor inputProcessor = new InputProcessor(sc, budget);
+        Printer.editMessage();
+
+        Transaction transaction = inputProcessor.getTransactionById();
+        Transaction newTransaction;
+
+        if (transaction instanceof Income oldIncome) {
+            newTransaction = inputProcessor.getEditedIncome(oldIncome);
+        } else {
+            Expense oldExpense = (Expense) transaction;
+            newTransaction = inputProcessor.getEditedExpense(oldExpense);
+        }
+
+        budget.updateTransaction(newTransaction);
+    }
+
+    private static void printAllTransactions(Budget budget) {
+        Printer.print(budget.getIncomes(), "Incomes");
+        Printer.print(budget.getExpenses(), "Expenses");
+    }
+}
